e76c802348c5461bc095eae742b1ebc7edbc7316
Cobus Kruger <cobus@bateleur.io>
2020-04-29 11:49:57 +0200

    1.0.7

--- src/cli.ts
+++ src/cli.ts
@@ -3,32 +3,34 @@
 import { combineFiles } from './index';
 
 function readParams() {
-    const value = process.argv.slice(1).reduce((obj, current) => {
-        const parts = current.split('=');
-        if (!parts || parts.length < 2) {
-            return obj;
-        }
-        return {
-            ...obj,
-            [parts[0]]: parts[1],
-        }
-    }, {});
-    return value;
+  const value = process.argv.slice(1).reduce((obj, current) => {
+    const parts = current.split('=');
+    if (!parts || parts.length < 2) {
+      return obj;
+    }
+    return {
+      ...obj,
+      [parts[0]]: parts[1],
+    };
+  }, {});
+  return value;
 }
 function processCommandLine() {
-    const params = readParams();
-    const output = params['--output'] || './output.sql';
-    const input = params['--input'] || '.';
-    const glob = params['--glob'] || '**/*.sql';
-    return {
-        glob, input, output,
-    };
+  const params = readParams();
+  const output = params['--output'] || './output.sql';
+  const input = params['--input'] || '.';
+  const glob = params['--glob'] || '**/*.sql';
+  return {
+    glob,
+    input,
+    output,
+  };
 }
 
 function run(params) {
-    combineFiles([params.input], {
-        fileGlobs: [params.glob],
-        outputFileName: params.output
-    });
+  combineFiles([params.input], {
+    fileGlobs: [params.glob],
+    outputFileName: params.output,
+  });
 }
 run(processCommandLine());
